1d9c8fc8e4850aa4840878757b4c91918df0b55c
NickSpangler <Spangleyman@mac.com>
2020-07-29 16:33:52 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+def prime?(i)
+    if i <= 1
+        false
+    else
+        range = (2...i).to_a
+        range.none? do |num|
+            i % num == 0
+        end
+    end
+end
